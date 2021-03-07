e47aa7db2516c1ce0b7401ff3cc0890e76bd47f8
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-03-06 20:06:01 -0500

    adding basic files wk6 pt1

--- routes/api.js
+++ routes/api.js
@@ -19,4 +19,23 @@ router.get("/users", (req, res) => {
      res.json({message: "all users route"});
 })
 
+router.get("/movies", (req, res) => {
+    // run a SQL query here -> get all movies from my DB
+    // res.json(query result here)
+    
+    // echo a message ->just like PHP
+    res.json({message: "all movies route"});
+})
+
+// dynamic route handler that can accept a parameter
+// this is equivalent to $_GET["id"]  => (req.params.id)
+// you're passing the id via the route: /api/movies/1, /api/movies/20, etc
+router.get("/movies/:id", (req, res) => {
+    // run a SQL query here -> get all movies from my DB
+    // res.json(query result here)
+    
+    // echo a message ->just like PHP
+    res.json({message: "get one movie route", movie: req.params.id });
+})
+
 module.exports = router;
