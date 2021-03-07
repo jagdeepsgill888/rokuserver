0407c94d5d1c2cac4653dddcbb0f614ed7e3dc65
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-03-06 21:44:39 -0500

    end of wk6

---
+++ config/userConfig.js
@@ -0,0 +1,10 @@
+// This is our SQL connect information
+const sqlCreds = {
+    user: "root",
+    password: "",
+    host: "localhost",
+    port: 3306,
+    database: "a4_w20_roku"
+}
+
+module.exports = sqlCreds;
