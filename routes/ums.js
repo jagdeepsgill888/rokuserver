7e4fa64df37d2acf063e48101c6478b99d600955
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2021-03-24 15:36:16 -0400

    end of wk10

--- routes/ums.js
+++ routes/ums.js
@@ -8,7 +8,7 @@ router.use(express.urlencoded({extended: false}));
 
 router.post('/admin/login', (req, res) => {
     // console.log(req.body.username, req.body.password);
-    connect.query(`SELECT user_id, user_admin, user_access FROM tbl_user  WHERE user_name = "${req.body.username}"`, (err, row) => {
+    connect.query(`SELECT user_id, user_admin, user_access, user_avatar FROM tbl_user  WHERE user_name = "${req.body.username}"`, (err, row) => {
         if (err) throw err;
 
         if (row.length) {
@@ -20,7 +20,7 @@ router.post('/admin/login', (req, res) => {
 })
 
 router.get('/admin/getusers', (req, res) => {
-    connect.query('SELECT user_id, user_name, user_admin, user_access FROM tbl_user', (err, users) => {
+    connect.query('SELECT user_id, user_name, user_admin, user_access, user_avatar FROM tbl_user', (err, users) => {
         if (err) throw err;
 
         res.status(200).json(users);
